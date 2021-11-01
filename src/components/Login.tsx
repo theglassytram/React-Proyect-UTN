e56c9359cec167e84415ddd97ea1ffa8ef79d6bd
unknown <rapallinim.89@gmail.com>
2021-11-01 17:56:06 -0300

    first commit

---
+++ src/components/Login.tsx
@@ -0,0 +1,14 @@
+import {useState} from 'react';
+import React from 'react';
+
+function Login() {
+
+    return (
+        <>
+        <h3>Login</h3>
+        <input type="text"></input>
+        </>
+    )
+}
+
+export default Login;
