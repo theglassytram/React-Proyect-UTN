f1d17a54939a5d76604f1eb55b634bc0e8cc6577
Claudio Sanchez <claudio.sanchez.so@softtekosde.com>
2021-11-15 18:34:07 -0300

    add redux

---
+++ src/redux/reducers/userReducers.tsx
@@ -0,0 +1,31 @@
+import { CREATE_USER, LOGOUT_USER, SIGNIN_USER, SETDATA_USER } from "../../config/constans";
+import { AuthAction }  from "../store";
+import { IUser } from "../../interfaces/users/user";
+
+const initialStateUser: IUser = {uid:"",authenticated:false,json_stringify:"",favorites:[],numberpage:1};
+
+export default (state = initialStateUser, action: AuthAction) => {
+  switch(action.type) {
+    case CREATE_USER:
+        return {
+        ...state
+    }
+    case SIGNIN_USER:
+        return {
+        ...state,
+        authenticated:action.payload
+    }
+    case LOGOUT_USER:
+        return {
+        ...state,
+        authenticated:action.payload
+    }
+    case SETDATA_USER:
+        return {
+        ...state,
+        json_stringify:action.payload
+    }  
+    default: 
+      return state;
+  }
+}
